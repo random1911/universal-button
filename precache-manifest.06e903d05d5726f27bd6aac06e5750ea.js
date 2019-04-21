117b9415a46a5b41e0e1d0076ae5b272073a77a4
Alexander Ivanov <ivanov@cindicator.com>
2019-04-21 11:41:13 +0300

    Updates

---
+++ precache-manifest.06e903d05d5726f27bd6aac06e5750ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4291909f80e6841691ffb5501eab63b",
+    "url": "/universal-button/static/media/products-button-change.a4291909.svg"
+  },
+  {
+    "revision": "462e83ed11f170c2c1b0adb871c742be",
+    "url": "/universal-button/static/media/expand-20.462e83ed.svg"
+  },
+  {
+    "revision": "a9fe8aa05fac918231669f2ae8c22ea2",
+    "url": "/universal-button/static/media/collapse-20.a9fe8aa0.svg"
+  },
+  {
+    "revision": "f71483d2936fedc5dc50cf7f98e39ff2",
+    "url": "/universal-button/static/media/12x12-block.f71483d2.svg"
+  },
+  {
+    "revision": "0de3fd3ea51806887ca1",
+    "url": "/universal-button/static/js/runtime~main.012becf8.js"
+  },
+  {
+    "revision": "986ad002a794234301fb",
+    "url": "/universal-button/static/js/main.0a5d2481.chunk.js"
+  },
+  {
+    "revision": "51964eb80de0dd57b06b",
+    "url": "/universal-button/static/js/2.85156c68.chunk.js"
+  },
+  {
+    "revision": "986ad002a794234301fb",
+    "url": "/universal-button/static/css/main.72b4996c.chunk.css"
+  },
+  {
+    "revision": "70e092b5a895f2eda1fd9dd2fca05419",
+    "url": "/universal-button/index.html"
+  }
+];
